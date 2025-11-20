6500619a21bb168f034800c93f963a16de4b4bee
omdhakal1011-stack <omdhakal1011@gmail.com>
2025-11-20 17:42:40 +0545

    Add Firebase configuration to firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWboy0BRSAYU7nNXUQY3OeWh8kWDpFtfQ",
+  authDomain: "cozy-gaming-community.firebaseapp.com",
+  databaseURL: "https://cozy-gaming-community-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cozy-gaming-community",
+  storageBucket: "cozy-gaming-community.firebasestorage.app",
+  messagingSenderId: "340605252846",
+  appId: "1:340605252846:web:2d2cc847b09dda9d62afff",
+  measurementId: "G-4YGM5NCYMN"
+};
+firebase.initializeApp(firebaseConfig);
